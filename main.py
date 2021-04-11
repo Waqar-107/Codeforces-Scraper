a815204ff4934f4e3add51985bcadad5c219e74a
Waqar-107 <waqar.hassan866@gmail.com>
2021-04-12 03:02:45 +0600

    new class added for keeping submission

--- main.py
+++ main.py
@@ -4,16 +4,32 @@ import re
 from util import *
 
 
+class CodeInfo:
+    def __init__(self, submissionID, verdict, time, memory, name, code, language):
+        pass
+
+
 class CodeforcesScraper:
     def __init__(self, user_name, page_required):
         self.user_Name = user_name
         self.page_required = page_required
         self.base_url = "http://codeforces.com"
+        self.src_code_div_id = "program-source-text"
 
     @staticmethod
     def fetch_data(url):
         page = urlopen(url)
         return page
+
+    @staticmethod
+    def fetch_code(url):
+        page = urlopen(url)
+        soup = BeautifulSoup(page, "html.parser")
+        code = soup.find(id="program-source-text")
+
+        # split the lines, they already has \r so writing the lines will create lines
+        code_chunks = code.text.split("\n")
+        return code_chunks
 
     # comparison can be either "time" or "memory"
     # by default it is time.
@@ -24,6 +40,8 @@ class CodeforcesScraper:
             soup = BeautifulSoup(page, "html.parser")
             table = soup.find_all("table")
             rows = table.find_all("tr")
+
+            accepted_codes = {}
 
             for row in rows:
                 column = row.find_all("td")
@@ -41,31 +59,3 @@ class CodeforcesScraper:
 
                 if re.match(verdictList["ac"], verdict):
                     pass
-
-
-
-
-
-# username = "Fuad113"
-# total_page = 4
-#
-url = "http://codeforces.com/submissions/_lucifer_/page/1011"
-
-page = urlopen(url)
-soup = BeautifulSoup(page, "html.parser")
-table = soup.find_all("table")
-rows = table[0].find_all("tr")
-column = rows[1].find_all("td")
-
-submissionLink = column[0].find("a")["href"]
-problemName = column[3].find("a").text.strip()
-language = column[4].text.strip()
-verdict = column[5].text.strip()
-
-# time in ms
-time = int(column[6].text.strip().split("\xa0")[0])
-
-# memory in KB
-memory = int(column[7].text.strip().split("\xa0")[0])
-
-print(submissionLink)
